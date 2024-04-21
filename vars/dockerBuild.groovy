41d88dffe9356c05be2f3856eba222c57e025a3f
viraj <virajmadusanka99999@gmail.com>
2024-04-21 23:58:40 +0530

    new

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String dockerHubUsername, String imageName) {
-    sh "ls"
+    sh "docker run hello-world"
     // Build the Docker image
     sh "docker build --build-arg REACT_APP_RAPID_API_KEY=869be490d8mshaedf30c3e5c2ca2p1011afjsn7c2f0ce295c6 -t ${imageName} ."
      // Tag the Docker image
