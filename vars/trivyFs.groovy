7f9292f1986b270088fb6b7e29b2ed702876f8e9
viraj <virajmadusanka99999@gmail.com>
2024-04-22 20:16:28 +0530

    add file

--- vars/trivyFs.groovy
+++ vars/trivyFs.groovy
@@ -1,3 +1,4 @@
 def call() {
     sh 'trivy fs . > trivyfs.txt'
+    sh "trivy fs --format html -o ${env.WORKSPACE}/trivy-fs-report.html ."  // HTML report
 }
--- vars/trivyImage.groovy
+++ vars/trivyImage.groovy
@@ -1,3 +1,4 @@
 def call() {
     sh 'trivy image sevenajay/youtube:latest > trivyimage.txt'
+    sh "trivy image --format html -o ${env.WORKSPACE}/trivy-image-report.html sevenajay/youtube:latest"  // HTML report
 }
